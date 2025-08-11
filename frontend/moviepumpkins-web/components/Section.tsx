52b4f6015e7b4eb42fc2b701009c3c64f44852a1
hrobi <hrobiugy@gmail.com>
2025-08-11 21:55:15 +0200

    #26: Make routes that need login redirect to login page 26: make routes that need login redirect to login page

--- frontend/moviepumpkins-web/components/Section.tsx
+++ frontend/moviepumpkins-web/components/Section.tsx
@@ -16,7 +16,7 @@ export default function Section({
   return (
     <>
       <section
-        className={`xl:w-2/3 mx-auto rounded-md p-8 mt-5 bg-white border-gray-300 border-[1px] shadow-b-outline ${className}`}
+        className={`content-section rounded-md p-8 bg-white border-gray-300 border-[1px] shadow-b-outline ${className}`}
       >
         <h1 className="flex flex-row flex-wrap items-center gap-2 text-3xl border-l-5 border-primary pl-5 font-bold mb-3">
           {title}
