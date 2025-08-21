78b8d6336383c565c5f30779d2ddd615a0e33995
hrobi <hrobiugy@gmail.com>
2025-08-21 22:43:03 +0200

    frontend changes #35: [BACKEND] create model
    -- in progress --
    35: backend create model
    HEAD

---
+++ backend-core/src/main/kotlin/net/moviepumpkins/core/media/mediadetails/db/SeriesDetailsEntity.kt
@@ -0,0 +1,39 @@
+package net.moviepumpkins.core.media.mediadetails.db
+
+import jakarta.persistence.DiscriminatorValue
+import jakarta.persistence.Entity
+
+@Entity
+@DiscriminatorValue(MEDIA_TYPE_SERIES)
+class SeriesDetailsEntity(
+    id: Long?,
+    title: String,
+    shortDescription: String,
+    directors: List<String>,
+    writers: List<String>,
+    actors: List<String>,
+    releaseYear: Int,
+    originalTitle: String?,
+    country: String?,
+    awards: List<String>,
+    awardsWinCount: Int?,
+    nominationsCount: Int?,
+    seasonCount: Int,
+    episodeCount: Int,
+    endYear: Int?
+) : MediaDetailsEntity(
+    id = id,
+    title = title,
+    shortDescription = shortDescription,
+    directors = directors,
+    writers = writers,
+    actors = actors,
+    releaseYear = releaseYear,
+    originalTitle = originalTitle,
+    country = country,
+    awards = awards,
+    awardsWinCount = awardsWinCount,
+    nominationsCount = nominationsCount,
+    otherDetails = MediaAdditionalDetails.ForSeries(seasonCount, episodeCount, endYear),
+    mediaType = MediaType.MOVIE
+)
