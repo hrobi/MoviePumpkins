2e5912dfe610e0539bce002bed44a5fcfb7b8d75
hrobi <hrobiugy@gmail.com>
2025-10-02 15:07:28 +0200

    #44: frontend fix authentication 44: frontend fix authentication

--- frontend/moviepumpkins-web/configuration/auth/config/NextAuthConfig.ts
+++ frontend/moviepumpkins-web/configuration/auth/config/NextAuthConfig.ts
@@ -1,80 +1,69 @@
-import {
-    createOwnUser,
-    CreateOwnUserErrors,
-    CreateOwnUserResponses,
-} from "@/_generated/configuration/api-client/clients/pumpkins-core";
 import { pumpkinsClient } from "@/configuration/api-client/clients";
-import { signOutOfKeycloak } from "@/configuration/auth/lib";
+import { refreshAccessToken, signOutOfKeycloak } from "@/configuration/auth/lib";
 import { ERROR_PAGE_ROUTE, SIGN_IN_PAGE_ROUTE } from "@/configuration/auth/model";
-import { unexpectedNoAuthError } from "@/features/app/lib";
 import { AuthOptions } from "next-auth";
 import Keycloak from "next-auth/providers/keycloak";
-import { cases, matchHttpResponse, total } from "toori";
+import { getUserRoleFromBackend } from "../lib/PumpkinsBackendCalls";
 
 export const nextAuthOptions: AuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        Keycloak({
-            issuer: process.env.OAUTH_ISS,
-            clientId: process.env.OAUTH_CLIENT_ID!,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET!,
-        }),
-    ],
-    callbacks: {
-        async jwt({token, account, profile}) {
-            if (account) {
-                token.id_token = account.id_token;
-                token.access_token = account.access_token;
-                token.provider = account.provider;
-            }
-            if (profile) {
-                token.username = profile.preferred_username;
-            }
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    Keycloak({
+      issuer: process.env.OAUTH_ISS,
+      clientId: process.env.OAUTH_CLIENT_ID!,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile, user }) {
+      if (account && user && profile) {
+        pumpkinsClient.setConfig({ auth: account.access_token });
+        try {
+          const role = await getUserRoleFromBackend();
+          return {
+            accessToken: account.access_token!,
+            accessTokenExpiresAt: account.expires_at! * 1000,
+            idToken: account.id_token!,
+            refreshToken: account.refresh_token!,
+            provider: account.provider!,
+            username: profile.preferred_username!,
+            role,
+          };
+        } catch (ex) {
+          signOutOfKeycloak(account.id_token!, nextAuthOptions);
+          throw ex;
+        }
+      }
 
-            pumpkinsClient.setConfig({
-                auth: token.access_token,
-            });
+      pumpkinsClient.setConfig({ auth: token.accessToken });
 
-            try {
-                console.log("[NextAuthConfig] createOwnUser");
-                const createOwnUserResp = await createOwnUser();
-                const userRole = matchHttpResponse<CreateOwnUserResponses & CreateOwnUserErrors>(
-                    createOwnUserResp,
-                )(
-                    cases(201, 200, ({body: {role}}) => role),
-                    total({
-                        401: ({response: {status}}) => {
-                            throw unexpectedNoAuthError(status);
-                        },
-                    }),
-                );
+      if (Date.now() < token.accessTokenExpiresAt) {
+        return token;
+      }
 
-                token.role = userRole;
-            } catch (ex) {
-                await signOutOfKeycloak(token, nextAuthOptions);
-                throw ex;
-            }
-
-            return token;
-        },
-        async session({token, session}) {
-            if (session.user && token.role) {
-                session.user.role = token.role;
-                session.user.username = token.username!;
-                session.user.accessToken = token.access_token!;
-            }
-            return session;
-        },
+      const newTokenSet = await refreshAccessToken(token, nextAuthOptions);
+      pumpkinsClient.setConfig({ auth: newTokenSet.accessToken });
+      return newTokenSet;
     },
-    events: {
-        async signOut({token}) {
-            if (token.provider === "keycloak") {
-                await signOutOfKeycloak(token, nextAuthOptions);
-            }
-        },
+    async session({ token, session }) {
+      if (session.user && token.role) {
+        session.user.username = token.username;
+        session.user.role = token.role;
+        session.user.accessToken = token.accessToken;
+        session.error = token.error;
+      }
+      return session;
     },
-    pages: {
-        signIn: SIGN_IN_PAGE_ROUTE,
-        error: ERROR_PAGE_ROUTE,
+  },
+  events: {
+    async signOut({ token }) {
+      if (token.provider === "keycloak") {
+        await signOutOfKeycloak(token.idToken, nextAuthOptions);
+      }
     },
+  },
+  pages: {
+    signIn: SIGN_IN_PAGE_ROUTE,
+    error: ERROR_PAGE_ROUTE,
+  },
 };
--- frontend/moviepumpkins-web/configuration/auth/lib/KeycloakCalls.ts
+++ frontend/moviepumpkins-web/configuration/auth/lib/KeycloakCalls.ts
@@ -3,13 +3,53 @@ import { JWT } from "next-auth/jwt";
 import { KeycloakProfile } from "next-auth/providers/keycloak";
 import { OAuthConfig } from "next-auth/providers/oauth";
 
-export async function signOutOfKeycloak(token: JWT, authOptions: AuthOptions) {
+export async function signOutOfKeycloak(idToken: string, authOptions: AuthOptions) {
   const issuerUrl = (
-    authOptions.providers.find(
-      (p) => p.id === "keycloak"
-    ) as OAuthConfig<KeycloakProfile>
+    authOptions.providers.find((p) => p.id === "keycloak") as OAuthConfig<KeycloakProfile>
   ).options!.issuer!;
   const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`);
-  logOutUrl.searchParams.set("id_token_hint", token.id_token!);
+  logOutUrl.searchParams.set("id_token_hint", idToken);
   await fetch(logOutUrl);
+}
+
+export async function refreshAccessToken(
+  token: JWT,
+  authOptions: AuthOptions
+): Promise<JWT> {
+  const options = (
+    authOptions.providers.find((p) => p.id === "keycloak") as OAuthConfig<KeycloakProfile>
+  ).options;
+
+  try {
+    const url = `${options!.issuer}/protocol/openid-connect/token`;
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+      body: new URLSearchParams({
+        client_id: options!.clientId,
+        client_secret: options!.clientSecret,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      }),
+    });
+
+    const refreshedTokens = await response.json();
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpiresAt: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    };
+  } catch (ex) {
+    console.error(ex);
+
+    return {
+      ...token,
+      error: "RefreshTokenError",
+    };
+  }
 }
