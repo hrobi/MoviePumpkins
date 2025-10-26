85a432592b36c3147e3945cd4e6d1febd3281d90
hrobi <hrobiugy@gmail.com>
2025-10-26 18:15:07 +0100

    #57: backend standardize #2 57: backend standardize 2 HEAD

--- frontend/moviepumpkins-web/configuration/api-client/config/PumpkinsOpenApiConfig.ts
+++ frontend/moviepumpkins-web/configuration/api-client/config/PumpkinsOpenApiConfig.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-    input: "./server-api.yml",
-    output: "./_generated/configuration/api-client/clients/pumpkins-core",
-    plugins: [
-        {
-            name: "@hey-api/client-next",
-            runtimeConfigPath: "./PumpkinsClientConfig.ts",
-        },
-    ],
+  input: "../../api/output/pumpkins-core-api.yml",
+  output: "./_generated/configuration/api-client/clients/pumpkins-core",
+  plugins: [
+    {
+      name: "@hey-api/client-next",
+      runtimeConfigPath: "./PumpkinsClientConfig.ts",
+    },
+  ],
 });
